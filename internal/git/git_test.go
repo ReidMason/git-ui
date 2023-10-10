52dd413b04de21027c74f63fdf348606ca4c15c7
Reid Mason <maddogshain132@gmail.com>
2023-10-10 19:02:48 +0100

    Added unit tests for directory and file creation

--- internal/git/git_test.go
+++ internal/git/git_test.go
@@ -7,14 +7,15 @@ import (
 )
 
 type MockGitCommandRunner struct {
-	diffRespponse string
+	diffResponse   string
+	statusResponse string
 }
 
 func (g MockGitCommandRunner) Stage(filepath string)          {}
 func (g MockGitCommandRunner) Unstage(filepath string)        {}
 func (g MockGitCommandRunner) Commit(commitMessage string)    {}
-func (g MockGitCommandRunner) GetDiff(filepath string) string { return g.diffRespponse }
-func (g MockGitCommandRunner) GetStatus() string              { return "" }
+func (g MockGitCommandRunner) GetDiff(filepath string) string { return g.diffResponse }
+func (g MockGitCommandRunner) GetStatus() string              { return g.statusResponse }
 
 func TestGetDiff(t *testing.T) {
 	testCases := []struct {
@@ -120,7 +121,7 @@ index 35b5809..4492ac6 100644
 	for _, tc := range testCases {
 		tc := tc
 
-		commandRunner := MockGitCommandRunner{diffRespponse: tc.rawDiff}
+		commandRunner := MockGitCommandRunner{diffResponse: tc.rawDiff}
 		git := New(commandRunner)
 		result := git.GetDiff("path/to/file")
 
@@ -128,7 +129,7 @@ index 35b5809..4492ac6 100644
 			resultDiffLine := result.Diff1[i]
 
 			if !cmp.Equal(expectedDiffLine, resultDiffLine) {
-				t.Fatalf("Diff1 failed line %d.\nExpected: '%v'\n     Got: '%v'", i+1, expectedDiffLine, resultDiffLine)
+				t.Fatalf("Diff1 failed test '%s' line %d.\nExpected: '%v'\n     Got: '%v'", tc.name, i+1, expectedDiffLine, resultDiffLine)
 			}
 		}
 
@@ -136,107 +137,166 @@ index 35b5809..4492ac6 100644
 			resultDiffLine := result.Diff2[i]
 
 			if expectedDiffLine.Content != resultDiffLine.Content {
-				t.Fatalf("Diff2 failed line %d.\nExpected: '%s'\n     Got: '%s'", i+1, expectedDiffLine.Content, resultDiffLine.Content)
+				t.Fatalf("Diff2 failed test '%s' line %d.\nExpected: '%s'\n     Got: '%s'", tc.name, i+1, expectedDiffLine.Content, resultDiffLine.Content)
 			}
 		}
 	}
 }
 
-//	func TestGetStatus(t *testing.T) {
-//		rawStatus := `# branch.oid c86e7ed35f16570194c2308a2f8cb53155d0440d
-//
-// # branch.head main
-// # branch.upstream origin/main
-// # branch.ab +0 -0
-// 1 .M N... 100644 100644 100644 51d742a142700c40e5d5d4915b44da5d238bef81 51d742a142700c40e5d5d4915b44da5d238bef81 internal/git/git.go
-// 1 .M N... 100644 100644 100644 8508f049bcb61d4c52d92e5a4c9a71051f00bcba 8508f049bcb61d4c52d92e5a4c9a71051f00bcba internal/git/git_test.go
-// 1 .M N... 100644 100644 100644 c789db6decaa4c7af3d5eb2214aea59f430dd5b1 c789db6decaa4c7af3d5eb2214aea59f430dd5b1 internal/utils/utils.go
-// 1 M. N... 100644 100644 100644 1cdd739f6591c3aca07eab977748142a1ba14056 c345bc6f17650da4f51350e8faa56e4f4c61663e main.go
-// ? internal/styling/styling.go`
-//
-//		result := *GetStatus(rawStatus)
-//
-//		expected := Directory{
-//			Name:     "Root",
-//			expanded: true,
-//			Parent:   nil,
-//			Files: []File{
-//				{
-//					Name:           "main.go",
-//					Dirpath:        ".",
-//					indexStatus:    77,
-//					workTreeStatus: 46,
-//				},
-//			},
-//			Directories: []*Directory{
-//				{
-//					Name:     "internal",
-//					expanded: true,
-//					Directories: []*Directory{
-//						{
-//							Name:        "git",
-//							Directories: make([]*Directory, 0),
-//							expanded:    true,
-//							Files: []File{
-//								{
-//									Name:           "git.go",
-//									Dirpath:        "internal/git",
-//									indexStatus:    46,
-//									workTreeStatus: 77,
-//								},
-//								{
-//									Name:           "git_test.go",
-//									Dirpath:        "internal/git",
-//									indexStatus:    46,
-//									workTreeStatus: 77,
-//								},
-//							},
-//						},
-//						{
-//							Name:        "utils",
-//							Directories: make([]*Directory, 0),
-//							expanded:    true,
-//							Files: []File{
-//								{
-//									Name:           "utils.go",
-//									Dirpath:        "internal/utils",
-//									indexStatus:    46,
-//									workTreeStatus: 77,
-//								},
-//							},
-//						},
-//					},
-//					Files: make([]File, 0),
-//				},
-//
-//				// {
-//				// 	Name:        "styling",
-//				// 	Directories: nil,
-//				// 	Files: []File{
-//				// 		{
-//				// 			Name:           "styling.go",
-//				// 			Dirpath:        "internal/styling",
-//				// 			IndexStatus:    46,
-//				// 			WorkTreeStatus: 77,
-//				// 		},
-//				// 	},
-//				// },
-//			},
-//		}
-//
-//		// checkDir(result, expected, t)
-//
-//		// if !cmp.Equal(result, expected) {
-//		// 	t.Fatal("Wrong file path output")
-//		// }
-//	}
+func TestGetStatus(t *testing.T) {
+	testCases := []struct {
+		name, rawStatus string
+		expected        Directory
+	}{
+		{
+			name: "Just status",
+			rawStatus: `# branch.oid c86e7ed35f16570194c2308a2f8cb53155d0440d
+
+# branch.head main
+# branch.upstream origin/main
+# branch.ab +0 -0
+1 M. N... 100644 100644 100644 1cdd739f6591c3aca07eab977748142a1ba14056 c345bc6f17650da4f51350e8faa56e4f4c61663e main.go`,
+			expected: func() Directory {
+				rootDir := Directory{
+					Name:     "Root",
+					Expanded: true,
+					Parent:   nil,
+					Files:    []File{},
+				}
+
+				file := File{
+					Name:           "main.go",
+					Dirpath:        ".",
+					Parent:         &rootDir,
+					IndexStatus:    'M',
+					WorkTreeStatus: '.',
+				}
+				rootDir.Files = append(rootDir.Files, file)
+
+				return rootDir
+			}(),
+		},
+		{
+			name: "Just status",
+			rawStatus: `# branch.oid c86e7ed35f16570194c2308a2f8cb53155d0440d
+
+# branch.head main
+# branch.upstream origin/main
+# branch.ab +0 -0
+1 .. N... 100644 100644 100644 1cdd739f6591c3aca07eab977748142a1ba14056 c345bc6f17650da4f51350e8faa56e4f4c61663e Directory/Internal/main.go
+1 MM N... 100644 100644 100644 1cdd739f6591c3aca07eab977748142a1ba14056 c345bc6f17650da4f51350e8faa56e4f4c61663e Directory/Internal/lib.go`,
+			expected: func() Directory {
+				rootDir := Directory{
+					Name:     "Root",
+					Expanded: true,
+					Parent:   nil,
+					Files:    []File{},
+				}
+
+				directory := Directory{
+					Name:     "Directory",
+					Expanded: true,
+					Parent:   &rootDir,
+					Files:    []File{},
+				}
+				rootDir.Directories = append(rootDir.Directories, &directory)
+
+				internalDir := Directory{
+					Name:     "Internal",
+					Expanded: true,
+					Parent:   &directory,
+					Files:    []File{},
+				}
+				directory.Directories = append(directory.Directories, &internalDir)
+
+				file := File{
+					Name:           "main.go",
+					Dirpath:        "Directory/Internal",
+					Parent:         &internalDir,
+					IndexStatus:    '.',
+					WorkTreeStatus: '.',
+				}
+				internalDir.Files = append(internalDir.Files, file)
+
+				lib := File{
+					Name:           "lib.go",
+					Dirpath:        "Directory/Internal",
+					Parent:         &internalDir,
+					IndexStatus:    'M',
+					WorkTreeStatus: 'M',
+				}
+				internalDir.Files = append(internalDir.Files, lib)
+
+				return rootDir
+			}(),
+		},
+	}
+
+	t.Parallel()
+	for _, tc := range testCases {
+		commandRunner := MockGitCommandRunner{statusResponse: tc.rawStatus}
+		git := New(commandRunner)
+
+		result := git.GetStatus()
+
+		checkDir(*result, tc.expected, t)
+	}
+}
+
 func checkDir(directory, expectedDirectory Directory, t *testing.T) {
-	if !cmp.Equal(directory, expectedDirectory) {
-		t.Fatalf("Directory mismatch. '%s' and '%s'", expectedDirectory.Name, directory.Name)
+	if directory.Name != expectedDirectory.Name {
+		t.Fatalf("Wrong name name for directory. Expected: '%s' Got: '%s'", expectedDirectory.Name, directory.Name)
+	}
+
+	if len(directory.Directories) != len(expectedDirectory.Directories) {
+		t.Fatalf("Wrong number of subdirectories for directory '%s'. Expected: '%d' Got: '%d'", directory.Name, len(expectedDirectory.Directories), len(directory.Directories))
+	}
+
+	if len(directory.Files) != len(expectedDirectory.Files) {
+		t.Fatalf("Wrong number of files for directory '%s'. Expected: '%d' Got: '%d'", directory.Name, len(expectedDirectory.Files), len(directory.Files))
+	}
+
+	if directory.Parent != nil {
+		if directory.Parent.Name != expectedDirectory.Parent.Name {
+			t.Fatalf("Wrong parent name for directory '%s'. Expected: '%s' Got: '%s'", directory.Name, expectedDirectory.Parent.Name, directory.Parent.Name)
+		}
+	} else if directory.Parent == nil && expectedDirectory.Parent != nil {
+		t.Fatalf("Expected no parent parent for directory '%s'.", directory.Name)
+	}
+
+	if directory.Expanded != directory.Expanded {
+		t.Fatalf("Wrong expanded value for directory '%s'. Expected: '%s' Got: '%s'", directory.Name, expectedDirectory.Parent.Name, directory.Parent.Name)
+	}
+
+	for i, expectedFile := range expectedDirectory.Files {
+		file := directory.Files[i]
+		checkFile(file, expectedFile, t)
 	}
 
 	for i, expectedSubDirectory := range expectedDirectory.Directories {
 		subDirectory := directory.Directories[i]
 		checkDir(*subDirectory, *expectedSubDirectory, t)
+	}
+}
+
+func checkFile(file, expectedFile File, t *testing.T) {
+	if file.Name != expectedFile.Name {
+		t.Fatalf("Wrong name for file. Expected: '%s' Got: '%s'", expectedFile.Name, file.Name)
+	}
+
+	if file.Parent.Name != expectedFile.Parent.Name {
+		t.Fatalf("Wrong parent name for file '%s'. Expected: '%s' Got: '%s'", file.Name, expectedFile.Parent.Name, file.Parent.Name)
+	}
+
+	if file.Dirpath != expectedFile.Dirpath {
+		t.Fatalf("Wrong dirpath for file '%s'. Expected: '%s' Got: '%s'", file.Name, expectedFile.Dirpath, file.Dirpath)
+	}
+
+	if file.IndexStatus != expectedFile.IndexStatus {
+		t.Fatalf("Wrong index status for file '%s'. Expected: '%d' Got: '%d'", file.Name, expectedFile.IndexStatus, file.IndexStatus)
+	}
+
+	if file.WorkTreeStatus != expectedFile.WorkTreeStatus {
+		t.Fatalf("Wrong work tree status for file '%s'. Expected: '%d' Got: '%d'", file.Name, expectedFile.WorkTreeStatus, file.WorkTreeStatus)
 	}
 }
