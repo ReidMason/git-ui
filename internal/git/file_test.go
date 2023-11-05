6b683b67aa62cdf6cbd362a9fe75819c0f38cf55
Reid Mason <maddogshain132@gmail.com>
2023-11-05 23:01:35 +0000

    Added test for file get filepath

--- internal/git/file_test.go
+++ internal/git/file_test.go
@@ -43,3 +43,69 @@ func TestDisplayName(t *testing.T) {
 		}
 	}
 }
+
+func TestGetFilePaths(t *testing.T) {
+	testCases := []struct {
+		name     string
+		expected string
+		file     File
+	}{
+		{
+			name: "Single file name",
+			file: File{
+				name:           "file.md",
+				secondName:     "oldfile.md",
+				Parent:         nil,
+				Dirpath:        "",
+				IndexStatus:    '.',
+				WorktreeStatus: '.',
+			},
+			expected: "file.md",
+		},
+		{
+			name: "Dirpath name display",
+			file: File{
+				name:           "file.md",
+				secondName:     "oldfile.md",
+				Parent:         nil,
+				Dirpath:        "directory/files",
+				IndexStatus:    '.',
+				WorktreeStatus: '.',
+			},
+			expected: "directory/files/file.md",
+		},
+		{
+			name: "Dirpath name display trailing slash",
+			file: File{
+				name:           "file.md",
+				secondName:     "oldfile.md",
+				Parent:         nil,
+				Dirpath:        "directory/files/",
+				IndexStatus:    '.',
+				WorktreeStatus: '.',
+			},
+			expected: "directory/files/file.md",
+		},
+		{
+			name: "Dirpath name display leading slash",
+			file: File{
+				name:           "file.md",
+				secondName:     "oldfile.md",
+				Parent:         nil,
+				Dirpath:        "/directory/files/",
+				IndexStatus:    '.',
+				WorktreeStatus: '.',
+			},
+			expected: "/directory/files/file.md",
+		},
+	}
+
+	t.Parallel()
+	for _, tc := range testCases {
+		output := tc.file.GetFilePath()
+
+		if output != tc.expected {
+			t.Fatalf("Failed to get filepath. Test %s expected '%s' got '%s'", tc.name, tc.expected, output)
+		}
+	}
+}
